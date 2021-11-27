ae96d7ed31266e174f419497e83452a27b82aca6
Mohan <notcoldum@gmail.com>
2021-11-27 18:12:08 -0500

    Open external endpoint in browser through python webbrowser

--- toolbox.py
+++ toolbox.py
@@ -1,13 +1,14 @@
-print("Welcome to Big data Processing Application")
-print("Please type the number that corresponds to which application you would like to run:")
-print("1. Apache Hadoop")
-print("2. Apache Spark")
-print("3. Jupyter Notebook")
-print("4. SonarQube and SonarScanner")
+import webbrowser
 
 while True:
+    print("Welcome to Big data Processing Application")
+    print("Please type the number that corresponds to which application you would like to run:")
+    print("1. Apache Hadoop")
+    print("2. Apache Spark")
+    print("3. Jupyter Notebook")
+    print("4. SonarQube and SonarScanner")
     print("\nType the number here > ")
-    while True:
-        pass
-    # appid = input()
-    # print(appid)
+    appid = input()
+    if appid == 3:
+        print("Opening Jupyter Notebook in web browser...\n")
+        webbrowser.open("http://34.123.215.223:8888", new = 0, autoraise = True)
